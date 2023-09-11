e2ebc3879a2877e8468095aea48ac40264107428
ThaliaOlvera <resets>
2023-09-11 14:04:47 -0500

    responsive modified

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Galia");
+
+const openCard = document.querySelectorAll(".openImg");
+const hideTitle = document.querySelector(".title");
+
+openCard.forEach((openCard) => {
+  openCard.addEventListener("click", () => {
+    removeActiveClasses();
+    openCard.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  openCard.forEach((openCard) => {
+    openCard.classList.remove("active");
+    hideTitle.classList.add("inactive");
+  });
+}
